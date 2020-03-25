649d789e1f42a71399e43014bfd7c701a39b3b0a
Randy Fay <randy@randyfay.com>
2020-03-25 11:12:25 -0600

    Improve settings.base.php "else if" => "elseif"
    Co-Authored-By: Amitai Burstein <amitai@gizra.com>

--- recipes/drupal8-multisite/web/sites/default/settings.base.php
+++ recipes/drupal8-multisite/web/sites/default/settings.base.php
@@ -51,6 +51,7 @@ $settings['class_loader_auto_detect'] = FALSE;
 // so it should work on any Drupal 8 or 9 version.
 if (defined('CONFIG_SYNC_DIRECTORY') && empty($config_directories[CONFIG_SYNC_DIRECTORY])) {
   $config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/files/sync';
-} else if (empty($settings['config_sync_directory'])) {
+} 
+elseif (empty($settings['config_sync_directory'])) {
   $settings['config_sync_directory'] = 'sites/default/files/sync';
 }
