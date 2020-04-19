d82fd3cbb33187793e183f4905903b2274abe342
Alban Jubert <ajubert@simplonprod.co>
2020-04-19 16:02:34 +0200

    Add demo project to demonstrate critical css extraction using webpack critical-css-webpack-plugin

--- recipes/puppeteer-headless-chrome-support/README.md
+++ recipes/puppeteer-headless-chrome-support/README.md
@@ -7,3 +7,45 @@ You can add Puppeteer support by setting the following extra Debian packages to 
 ```yaml
 webimage_extra_packages: [gconf-service, libasound2, libatk1.0-0, libcairo2, libgconf-2-4, libgdk-pixbuf2.0-0, libgtk-3-0, libnspr4, libpango-1.0-0, libpangocairo-1.0-0, libx11-xcb1, libxcomposite1, libxcursor1, libxdamage1, libxfixes3, libxi6, libxrandr2, libxrender1, libxss1, libxtst6, fonts-liberation, libappindicator1, libnss3, xdg-utils]
 ```
+
+## Critical CSS Demo
+
+You will find a proof of concept in the [demo](demo/) folder.
+
+It is based on the [Tailwind CSS Landing Page starter project](https://github.com/tailwindtoolbox/Landing-Page) and demonstrate how to use [Webpack Encore](https://symfony.com/doc/current/frontend.html) to compile javascript and css resources and extract critical CSS with critical-css-webpack-plugin from within the ddev web container.
+
+> Obviously, the PHP part is quite minimalistic and is just used to read the webpack manifest file to inject relevant javascript and css files and inline the critical css file in the landing page. However, the overall workflow can easily be adapted in a more complex PHP project using a CMS or a framework.
+
+### Startup
+
+For the root of the [demo](demo/) folder, start the ddev environment:
+```
+ddev start
+```
+
+The provided `.ddev/config.yaml` is pre-configured to include the needed webimage_extra_packages in order to run the critical-css-webpack-plugin.
+
+### Installation
+
+This project comes with a custom yarn command for convenience. To install the node dependancies, run:
+```
+ddev yarn install
+```
+
+### Generate the webpack assets and the critical CSS file
+
+Run
+```
+ddev yarn build
+```
+
+This will generate optimized javascript and css files in `web/webpack-assets` folder along with the critical css file in the `web/criticalcss` folder using the critical-css-webpack-plugin.
+
+Under the wood, critical-css-webpack-plugin will instantiate a headless Chrome using Puppeteer to access the landing page hosted by ddev in order to extact relevant critical css.
+
+At that stage you should be able to view the landing page at `https://demo.ddev.site`.
+
+> You can see how the plugin is triggered by opening the `webpack.config.js` configuration file.
+> A `yarn dev` and `yarn watch` scripts are also provided as examples.
+
+
---
+++ recipes/puppeteer-headless-chrome-support/demo/src/main.js
@@ -0,0 +1,84 @@
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var navcontent = document.getElementById("nav-content");
+var navaction = document.getElementById("navAction");
+var brandname = document.getElementById("brandname");
+var toToggle = document.querySelectorAll(".toggleColour");
+
+document.addEventListener('scroll', function () {
+
+    /*Apply classes for slide in bar*/
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 10) {
+        header.classList.add("bg-white");
+        navaction.classList.remove("bg-white");
+        navaction.classList.add("gradient");
+        navaction.classList.remove("text-gray-800");
+        navaction.classList.add("text-white");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-gray-800");
+            toToggle[i].classList.remove("text-white");
+        }
+        header.classList.add("shadow");
+        navcontent.classList.remove("bg-gray-100");
+        navcontent.classList.add("bg-white");
+    } else {
+        header.classList.remove("bg-white");
+        navaction.classList.remove("gradient");
+        navaction.classList.add("bg-white");
+        navaction.classList.remove("text-white");
+        navaction.classList.add("text-gray-800");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-white");
+            toToggle[i].classList.remove("text-gray-800");
+        }
+
+        header.classList.remove("shadow");
+        navcontent.classList.remove("bg-white");
+        navcontent.classList.add("bg-gray-100");
+
+    }
+
+});
+
+/*Toggle dropdown list*/
+/*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
+
+var navMenuDiv = document.getElementById("nav-content");
+var navMenu = document.getElementById("nav-toggle");
+
+document.onclick = check;
+
+function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+
+    //Nav Menu
+    if (!checkParent(target, navMenuDiv)) {
+        // click NOT on the menu
+        if (checkParent(target, navMenu)) {
+            // click on the link
+            if (navMenuDiv.classList.contains("hidden")) {
+                navMenuDiv.classList.remove("hidden");
+            } else {
+                navMenuDiv.classList.add("hidden");
+            }
+        } else {
+            // click both outside link and outside menu, hide menu
+            navMenuDiv.classList.add("hidden");
+        }
+    }
+
+}
+
+function checkParent(t, elm) {
+    while (t.parentNode) {
+        if (t == elm) {
+            return true;
+        }
+        t = t.parentNode;
+    }
+    return false;
+}
