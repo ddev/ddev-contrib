ab5f5f1c2f21346533c2e578f0b64a6702b5c00f
Mario Hernandez <1289805+mariohernandez@users.noreply.github.com>
2020-07-31 16:37:51 -0700

    Use stderr redirection to make "Import a sql dump if database is empty" quiet on start (#74)

--- hook-examples/import-db-if-empty/import-if-empty.sh
+++ hook-examples/import-db-if-empty/import-if-empty.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Use a table that should exist in your database instead of "mytable"
-if ! mysql -e 'SELECT * FROM mytable;' db > /dev/null; then
-  echo 'loading db'
+# Set TABLETOFIND to a table which should exist in a loaded database
+TABLETOFIND=users
+
+if ! mysql -e "SELECT * FROM ${TABLETOFIND};" db >/dev/null 2>&1; then
+  echo "loading database since table named '${TABLETOFIND}' was not found."
   # This assumes the db.sql.gz is in the root of your repository, but
   # adjust as necessary.
-  gzip -dc /var/www/html/db.sql.gz | mysql db
+  gzip -dc /var/www/html/.tarballs/d8composer.sql.gz | mysql db
+else
+  echo "NOT loading database; it already exists; table '${TABLETOFIND}' was found."
 fi
