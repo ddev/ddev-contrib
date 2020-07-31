8badb6be508517801ce560261cd36c19e4a86137
Randy Fay <randy@randyfay.com>
2020-07-31 17:20:34 -0600

    Hide both stdout and error outout

--- hook-examples/import-db-if-empty/import-if-empty.sh
+++ hook-examples/import-db-if-empty/import-if-empty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use a table that should exist in your database instead of "mytable"
-if ! mysql -e 'SELECT * FROM mytable;' db 2>/dev/null; then
+if ! mysql -e 'SELECT * FROM mytable;' db >/dev/null 2>&1; then
   echo 'loading db'
   # This assumes the db.sql.gz is in the root of your repository, but
   # adjust as necessary.
