3b9312ceac8e78a956f5ebd1fabfed964d10a26c
Benjamin Serfhos <benjamin@serfhos.com>
2021-01-15 18:16:13 +0100

    [TASK] Add documentation to add Laravel Horizon to DDEV

---
+++ recipes/laravel-horizon/config/database.php
@@ -0,0 +1,29 @@
+<?php
+return [
+// ..
+
+    /*
+    |--------------------------------------------------------------------------
+    | Redis Databases
+    |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer set of commands than a typical key-value systems
+    | such as APC or Memcached. Laravel makes it easy to dig right in.
+    |
+    */
+
+    'redis' => [
+
+        'client' => 'phpredis',
+
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+
+    ],
+// ..
+];
